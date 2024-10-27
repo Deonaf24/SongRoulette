5f6bd8701dcdc69f15a4c916f1dc200b3b6a001f
Deon Aftahi <deon.aftahi@gmail.com>
2024-10-26 20:30:10 -0400

    first commit

---
+++ Song Roulette/Song Roulette/Song_RouletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Song_RouletteApp.swift
+//  Song Roulette
+//
+//  Created by Deon Aftahi on 2024-10-26.
+//
+
+import SwiftUI
+
+@main
+struct Song_RouletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Song Roulette/Song RouletteTests/Song_RouletteTests.swift
@@ -0,0 +1,17 @@
+//
+//  Song_RouletteTests.swift
+//  Song RouletteTests
+//
+//  Created by Deon Aftahi on 2024-10-26.
+//
+
+import Testing
+@testable import Song_Roulette
+
+struct Song_RouletteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
